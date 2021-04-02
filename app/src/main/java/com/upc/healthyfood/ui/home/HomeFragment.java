02ddf4959517859b503ce435bf0b653f7421b0e0
jperaltao <jperalta@niubiz.com.pe>
2021-04-01 23:00:42 -0500

    Agregando Navegación y layout Mi Plan

---
+++ app/src/main/java/com/upc/healthyfood/ui/home/HomeFragment.java
@@ -0,0 +1,29 @@
+package com.upc.healthyfood.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.upc.healthyfood.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_my_plan, container, false);
+
+        return root;
+    }
+}
